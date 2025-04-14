08393a12bd6e535fe326d8c88abfc2dff218fe4e
Christopher O'Brien <4126049+chrisob55@users.noreply.github.com>
2025-04-14 13:31:55 +0100

    WIP

--- foo/Package.swift
+++ foo/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Foo",
-            url: "https://github.com/chrisob55/Embedded/releases/download/1.0.2/Foo.xcframework.zip",
+            url: "https://github.com/chrisob55/Embedded/releases/download/1.0.3/Foo.xcframework.zip",
             checksum: "a03a27e8ed0e8b8f94142c534f753f3e32c0d48ccbb1c550005bbc0fbee2bed9"
         )
     ]
