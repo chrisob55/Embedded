cf84d825a127a510e7d9f74cc557d187dc84d3cd
Christopher O'Brien <4126049+chrisob55@users.noreply.github.com>
2025-04-14 12:33:54 +0100

    Updated publishing

--- foo/build.gradle.kts
+++ foo/build.gradle.kts
@@ -5,13 +5,19 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    id("maven-publish")
+    id("signing")
 }
+
+group = "com.cob.foo"
+version = "1.0.0"
 
 kotlin {
     androidTarget {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_17)
         }
+        publishLibraryVariants("release")
     }
     iosX64()
     iosArm64()
@@ -50,5 +56,36 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/chrisob55/Embedded")
+            credentials {
+                username = System.getenv("GITHUB_USER_NAME")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        // Android publication
+        register<MavenPublication>("release") {
+            groupId = "com.cob.foo"
+            artifactId = "foo-android"
+            version = "1.0.0"
+        }
+
+        // iOS publication
+        create<MavenPublication>("ios") {
+            groupId = "com.cob.foo"
+            artifactId = "foo-ios"
+            version = "1.0.0"
+
+            from(components["kotlin"])
+        }
     }
 }
